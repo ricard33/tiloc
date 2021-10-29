ee647fe19306bb33b1b89cddbd9e5290c253e6dc
Cédric RICARD <ricard33@gmail.com>
2021-10-28 20:34:46 -0400

    Hide unaffected (=cancelled) bookings in "Next events" component

--- core/api.py
+++ core/api.py
@@ -130,11 +130,11 @@ class BookingViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['get'])
     def next_events(self, request, pk=None):
-        qs1 = models.Booking.objects.filter(begin_date__gte=timezone.now()).annotate(date=F('begin_date'),
+        qs1 = models.Booking.objects.filter(begin_date__gte=timezone.now(), lodging__isnull=False).annotate(date=F('begin_date'),
                                                                                      event_type=Value('CHECKIN')) \
             .values('id', 'date', 'guest_name', 'event_type', 'guest_name',
                     lodging_name=F('lodging__name'), booking_channel=F('source__name'))
-        qs2 = models.Booking.objects.filter(end_date__gte=timezone.now()).annotate(date=F('end_date'),
+        qs2 = models.Booking.objects.filter(end_date__gte=timezone.now(), lodging__isnull=False).annotate(date=F('end_date'),
                                                                                    event_type=Value('CHECKOUT')) \
             .values('id', 'date', 'guest_name', 'event_type', 'guest_name',
                     lodging_name=F('lodging__name'), booking_channel=F('source__name'))
