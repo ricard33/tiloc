f069e1ff40fa22dadda7d471d8e38a6dc7badf74
Cédric RICARD <ricard33@gmail.com>
2022-01-24 17:45:29 -0400

    Fix left menu appearance

--- frontend/src/layouts/Main/components/Sidebar/components/SidebarNav/SidebarNav.js
+++ frontend/src/layouts/Main/components/Sidebar/components/SidebarNav/SidebarNav.js
@@ -1,58 +1,91 @@
 /* eslint-disable react/no-multi-comp */
 /* eslint-disable react/display-name */
-import React, { forwardRef } from 'react';
-import { NavLink as RouterLink } from 'react-router-dom';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@mui/styles';
-import { List, ListItem, colors } from '@mui/material';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+import { makeStyles } from "@mui/styles";
+import { List, ListItem, colors } from "@mui/material";
 
 const useStyles = makeStyles(theme => ({
   root: {},
   item: {
-    display: 'flex',
+    display: "flex",
     paddingTop: 0,
     paddingBottom: 0
   },
   button: {
     color: colors.blueGrey[800],
-    padding: '10px 8px',
-    justifyContent: 'flex-start',
-    textTransform: 'none',
+    padding: "10px 8px",
+    justifyContent: "flex-start",
+    // textTransform: 'none',
     letterSpacing: 0,
-    width: '100%',
-    fontWeight: theme.typography.fontWeightMedium
+    width: "100%",
+    fontWeight: theme.typography.fontWeightMedium,
+
+    display: 'inline-flex',
+    position: 'relative',
+    boxSizing: 'border-box',
+    backgroundColor: 'transparent',
+    outline: '0px',
+    border: '0px',
+    margin: '0px',
+    cursor: 'pointer',
+    verticalAlign: 'middle',
+    appearance: 'none',
+    textDecoration: 'none',
+    fontSize: '0.8125rem',
+    lineHeight: 1.75,
+    textTransform: 'uppercase',
+    minWidth: '64px',
+    "&:hover": {
+      backgroundColor: theme.palette.hover,
+    }
   },
   icon: {
-    color: theme.palette.icon,
     width: 24,
     height: 24,
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     marginRight: theme.spacing(1)
   },
   active: {
     color: theme.palette.primary.main,
     fontWeight: theme.typography.fontWeightMedium,
-    '& $icon': {
-      color: theme.palette.primary.main
-    }
+  },
+  disabled: {
+    color: theme.palette.primary.light,
+    cursor: "default",
   }
 }));
 
-const CustomRouterLink = forwardRef((props, ref) => (
-  <div
-    ref={ref}
-    style={{ flexGrow: 1 }}
-  >
-    <RouterLink {...props} />
-  </div>
-));
+const CustomNavLink = (props) => {
+  const { className, disabled, ...rest } = props;
+
+  const handleClick = (e) => {
+    if(disabled) e.preventDefault()
+  }
+
+  return (
+    <NavLink
+      className={className}
+      style={{ flexGrow: 1 }}
+      onClick={handleClick}
+      {...rest}
+    />
+  );
+};
+
+CustomNavLink.propTypes = {
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+  disabled: PropTypes.bool,
+};
 
 const SidebarNav = props => {
   const { pages, className, ...rest } = props;
 
   const classes = useStyles();
+
 
   return (
     <List
@@ -65,16 +98,15 @@ const SidebarNav = props => {
           disableGutters
           key={page.title}
         >
-          <CustomRouterLink
-            className={({isActive}) => classes.button + (isActive ? (" " + classes.active) : "")}
-            target={page.external ? '_blank' : ''}
-            // component={CustomRouterLink}
+          <CustomNavLink
+            className={({ isActive }) => classes.button + (isActive ? (" " + classes.active) : "") + (page.disabled ? (" " + classes.disabled) : "")}
+            target={page.external ? "_blank" : ""}
             to={page.href}
             disabled={page.disabled}
           >
             <div className={classes.icon}>{page.icon}</div>
             {page.title}
-          </CustomRouterLink>
+          </CustomNavLink>
         </ListItem>
       ))}
     </List>
