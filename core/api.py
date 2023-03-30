96df25c506179f696ca01133c6ce0bae3e362855
Cédric RICARD <ricard33@gmail.com>
2023-03-30 07:28:18 -0400

    Remove deleted booking from guests list

--- core/api.py
+++ core/api.py
@@ -157,6 +157,7 @@ class BookingViewSet(viewsets.ModelViewSet):
     *,
     ROW_NUMBER() OVER(PARTITION BY guest_name ORDER BY begin_date DESC) AS row_number
   FROM core_booking
+  WHERE deleted = 0
 )
 SELECT
   id, guest_name as name, guest_contact as contact, guest_address as address
