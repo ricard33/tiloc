f2eab6a8536a635b8939921033dae140d144ab9d
Cédric RICARD <ricard33@gmail.com>
2024-02-05 23:18:41 -0400

    Revert urls change for index

--- location/urls.py
+++ location/urls.py
@@ -101,6 +101,6 @@ if settings.ENV == "dev":
                 -1, re_path(relative_path, never_cache(serve_static_file), kwargs={"document_path": fullpath})
             )
 
-urlpatterns.append(re_path(r"^$", never_cache(serve_static_file),
+urlpatterns.append(re_path(r"^", never_cache(serve_static_file),
                            kwargs={"document_path": os.path.join(settings.STATIC_ROOT, "index.html")},
                            name="home"))
