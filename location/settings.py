b96d8693940c82be03df14f51d895a6e67adab94
Cédric RICARD <ricard33@gmail.com>
2026-01-31 20:33:36 -0400

    Add missing cron job

--- location/settings.py
+++ location/settings.py
@@ -317,6 +317,7 @@ IMPORT_EXPORT_SKIP_ADMIN_LOG = True
 CRON_CLASSES = [
     "core.cron.SyncBookingsJob",
     "core.cron.ExportBookingsJob",
+    "core.cron.PurgeNotificationsJob",
     # ...
 ]
 DJANGO_CRON_DELETE_LOGS_OLDER_THAN = 30
